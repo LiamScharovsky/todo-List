197eee4a4536fef3357d6f1e5bcea4e279c1f203
LiamScharovsky <99295834+LiamScharovsky@users.noreply.github.com>
2022-05-11 09:47:15 -0500

    Delete arrowFunction.js

--- arrowFunction.js
+++
@@ -1,6 +0,0 @@
-let printName = (aString) => console.log(`Hello, ${aString}`)
-printName('Liam')
-
-
-let printMultipleName = (firstName, lastName) => { console.log(`Hello ${firstName}`); console.log(`${lastName}`) }
-printMultipleName('Liam', 'Scharovsky')
